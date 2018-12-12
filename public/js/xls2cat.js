4d0496708a6ad96e273689fe24b40d41c6d94068
lab37 <981920228@qq.com>
2018-12-12 13:40:59 +0800

    V1.0.1

--- public/js/xls2cat.js
+++ public/js/xls2cat.js
@@ -32,7 +32,7 @@ function catArr(xArr, yArrs) {
     rsts.push(tmp);
   }
   $("#" + xArr[0]).css("background-color", "green");
-  $("#" + xArr[0]).append("---------合并成功，并入数据：" + (yArrs.length-1) +"条");
+  $("#" + xArr[0]+"s").text("---------合并成功，并入数据：" + (yArrs.length-1) +"条");
 }
 
 
@@ -68,15 +68,30 @@ function xls2arr(file, fxarr) {
   if (rABS) reader.readAsBinaryString(file); else reader.readAsArrayBuffer(file);
 }
 document.querySelector('#i2').onchange = function () {
+  $("#monitor").hide();
   let files = this.files;
   fx = files[0];
+  $('#fname').text('对应表文件：'+fx.name);
+  $("#result").children().css("background-color", "#fbfaf9");
+  $("#result span").empty();
 }
 
 
 
 document.querySelector('#i1').onchange = function () {
+  $("#monitor").hide();
   // let newWB = XLSX.utils.book_new();
+  let countFiles=0;
+  $("#result").empty();
   files2cat = this.files;
+  for (let i in files2cat) {
+    if (typeof files2cat[i] == "object") {
+      countFiles=countFiles + 1;
+      $("#result").append("<li id=" + files2cat[i].name.substr(0, files2cat[i].name.indexOf(".")) + " >" + files2cat[i].name +"<span id=" + files2cat[i].name.substr(0, files2cat[i].name.indexOf(".")) + "s" + " >"+"</span>"+ "</li>");
+    }
+  }
+  $('#fcount').text('共选择文件：'+countFiles+'个');
+
 };
 
 
@@ -106,11 +121,10 @@ document.querySelector('#i3').onclick = function () {
     let sheetName = sheetNames[0];//只取第一个sheet
     let fxarrs = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName], { header: 1 });
 
-    $("#result").empty();
+    
 
     for (let i in files2cat) {
       if (typeof files2cat[i] == "object") {
-        $("#result").append("<li id=" + files2cat[i].name.substr(0, files2cat[i].name.indexOf(".")) + " >" + files2cat[i].name + "</li>");
         let ct=0;
         for (let y = 0; y < fxarrs.length; y++) {
           if (fxarrs[y][0] == files2cat[i].name.substr(0, files2cat[i].name.indexOf("."))) {
